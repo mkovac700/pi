d72808fc18abad52aed11628dfda2b1515cf1c0e
Marijan Kovač <mkovac@foi.hr>
2022-07-02 01:10:54 +0200

    Prijava i registracija
    Prvotna verzija funkcionalnosti: prijava u sustav uz autentifikaciju, upravljanje korisnickim razinama

--- Software/MiniStore/GlavnaForma.cs
+++ Software/MiniStore/GlavnaForma.cs
@@ -12,7 +12,7 @@ namespace MiniStore
 {
     public partial class GlavnaForma : Form
     {
-        public int KorisnikId { get; set; } = 1;
+        Trgovina trenutnaTrgovina;
         int trenutnaTrgovinaId;
         public GlavnaForma()
         {
@@ -73,21 +73,65 @@ namespace MiniStore
 
         private void GlavnaForma_Load(object sender, EventArgs e)
         {
-           
-            using (var db = new Database())
-            {
-                var trenutniKorisnik = db.Korisniks.Include("Trgovinas").FirstOrDefault((k) => k.id == KorisnikId);
-
-                if (trenutniKorisnik != null) {
-                    var trenutnaTrgovina = trenutniKorisnik.Trgovinas.ToList().First();
+            if (Autentifikator.DohvatiPrijavljenogKorisnika() != null) {
+                trenutnaTrgovina = Autentifikator.DohvatiPrijavljenogKorisnika().Trgovinas.FirstOrDefault();
+                if (trenutnaTrgovina != null)
+                {
                     trenutnaTrgovinaId = trenutnaTrgovina.id;
-
                     lblPoslovnica.Text = $"Poslovnica: {trenutnaTrgovina.oznaka}";
                 }
-            }
+                else
+                {
+                    lblPoslovnica.Text = $"Poslovnica: nema";
+                }
+                
+                lblKorisnik.Text = $"Korisnik: {Autentifikator.DohvatiPrijavljenogKorisnika().ime} {Autentifikator.DohvatiPrijavljenogKorisnika().prezime}";
+
+                PostaviGumbove();
+            } 
            
         }
 
-        
+        private void btnOdjava_Click(object sender, EventArgs e)
+        {
+            Autentifikator.OdjaviKorisnika();
+            
+            this.Close();
+        }
+
+        private void timerTrenutnoVrijeme_Tick(object sender, EventArgs e)
+        {
+            lblVrijeme.Text = DateTime.Now.ToString("dd. MMMM yyyy. HH:mm:ss");
+        }
+
+        private void PostaviGumbove()
+        {
+            foreach(var control in tbpGumbi.Controls)
+            {
+                (control as Button).Enabled = false;
+            }
+            
+            int razina = Autentifikator.DohvatiRazinuPrijavljenogKorisnika();
+
+            if(razina <= 3) //zaposlenik
+            {
+                btnRacun.Enabled = true;
+                btnNarucivanjeArtikala.Enabled = true;
+                btnZaprimanjeArtikala.Enabled = true;
+                btnIzradaPovratnice.Enabled = true;
+                btnUpravljanjeSatnicom.Enabled = true;
+            }
+            if(razina <= 2) //voditelj
+            {
+                btnUpravljanjeArtiklima.Enabled = true; //funkcionalnost podijeliti na dvije opcije: voditelj moze obavljati CRUD nad ArtiklSkladiste, dok superuser nad Artikl + ArtiklCijena
+                btnInventura.Enabled = true;
+                btnStatistikaProdaje.Enabled = true;
+                //postavke (dodati gumb)
+            }
+            if (razina == 1) //admin
+            {
+                //postavke (dodati gumb)
+            }
+        }
     }
 }
