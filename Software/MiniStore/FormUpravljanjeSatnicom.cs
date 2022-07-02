470d76ccd97d96b10671aa6ccfd172aa55496cb4
Marijan Kovač <mkovac@foi.hr>
2022-07-02 23:04:08 +0200

    Upravljanje satnicom
    Izmjene na formi (popravci u odnosu na pocetno postavljanje)

--- Software/MiniStore/FormUpravljanjeSatnicom.cs
+++ Software/MiniStore/FormUpravljanjeSatnicom.cs
@@ -20,20 +20,48 @@ namespace MiniStore
 
         private void SetMyCustomFormat()
         {
-            dateTimePicker1.CustomFormat = "MMMM yyyy";
-            //dateTimePicker1.ShowUpDown = true;
+            dtpMjesec.CustomFormat = "MMMM yyyy";
+            dtpMjesec.ShowUpDown = true;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnDodaj_Click(object sender, EventArgs e)
         {
             FormSatnica formSatnica = new FormSatnica();
             formSatnica.ShowDialog();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnUredi_Click(object sender, EventArgs e)
         {
             FormSatnica formSatnica = new FormSatnica();
             formSatnica.ShowDialog();
+        }
+
+        private void FormUpravljanjeSatnicom_Load(object sender, EventArgs e)
+        {
+            PostaviGumbove();
+        }
+
+        private void PostaviGumbove()
+        {
+            foreach(var item in this.Controls)
+            {
+                if(item.GetType() == typeof(Button))
+                {
+                    (item as Button).Enabled = false;
+                }
+            }
+
+            if(Autentifikator.DohvatiRazinuPrijavljenogKorisnika() <= 3) //zaposlenik moze samo dodavati zapis i ispisati izvjesce
+            {
+                btnDodaj.Enabled = true;
+                btnIspis.Enabled = true;
+            }
+
+            if(Autentifikator.DohvatiRazinuPrijavljenogKorisnika() <= 2) //admin trgovine kao i superuser moze sve ostalo
+            {
+                btnUredi.Enabled = true;
+                btnObrisi.Enabled = true;
+            } 
         }
     }
 }
